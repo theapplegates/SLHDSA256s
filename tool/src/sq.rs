theapplegates/SLHDSA256s

commit b4a98c05d39035e947415c5ab1172280619084f3

lib: Move the key store into struct Sequoia.

tool/src/sq.rs
@@ -13,8 +13,6 @@ use typenum::Unsigned;
 use anyhow::anyhow;
 use anyhow::Context as _;
 
-use once_cell::sync::OnceCell;
-
 use sequoia::openpgp;
 use openpgp::Cert;
 use openpgp::crypto;
@@ -44,7 +42,7 @@ use cert_store::store::UserIDQueryParams;
 use sequoia::wot;
 use wot::store::Store as _;
 
-use sequoia_keystore as keystore;
+use sequoia::key_store as keystore;
 use keystore::Protection;
 
 use sequoia::Sequoia;
@@ -57,7 +55,6 @@ use crate::cli::types::SpecialName;
 use crate::cli::types::StdinWarning;
 use crate::cli::types::cert_designator;
 use crate::cli::types::key_designator;
-use crate::cli::types::paths::StateDirectory;
 use crate::common::password;
 use crate::common::ui;
 use crate::output::hint::Hint;
@@ -137,10 +134,6 @@ pub struct Sq {
 
     pub policy_as_of: SystemTime,
 
-    // The key store.
-    pub key_store_path: Option<StateDirectory>,
-    pub key_store: OnceCell<Mutex<keystore::Keystore>>,
-
     /// A password cache.  When encountering a locked key, we first
     /// consult the password cache.  The passwords are only tried if
     /// it is safe.  That is, the passwords are only tried if we are
@@ -175,13 +168,6 @@ impl Sq {
     /// Be quiet.
     pub fn quiet(&self) -> bool {
         self.config.quiet()
-    }
-
-    /// Returns whether the key store is disabled.
-    fn no_key_store(&self) -> bool {
-        self.key_store_path.as_ref()
-            .map(|s| s.is_none())
-            .unwrap_or(self.sequoia.stateless())
     }
 
     /// Returns the cert store's base directory, if it is enabled.
@@ -227,40 +213,14 @@ impl Sq {
     ///
     /// If the key store is disabled, returns `Ok(None)`.
     pub fn key_store_path(&self) -> Result<Option<PathBuf>> {
-        let default = || {
-            Ok(self.sequoia.home()
-               .map(|h| h.data_dir(sequoia::directories::Component::Keystore)))
-        };
-
-        if let Some(dir) = self.key_store_path.as_ref() {
-            match dir {
-                StateDirectory::Absolute(p) => Ok(Some(p.clone())),
-                StateDirectory::Default => default(),
-                StateDirectory::None => Ok(None),
-            }
-        } else {
-            default()
-        }
-    }
-
-    fn no_key_store_err() -> clap::Error {
-        clap::Error::raw(clap::error::ErrorKind::ArgumentConflict,
-                         "Operation requires a key store, \
-                          but the key store is disabled")
+        self.sequoia.key_store_path()
     }
 
     /// Returns the key store's path.
     ///
     /// If the key store is disabled, returns an error.
     pub fn key_store_path_or_else(&self) -> Result<PathBuf> {
-        if self.no_key_store() {
-            Err(Self::no_key_store_err().into())
-        } else {
-            self.key_store_path()?
-                .ok_or_else(|| {
-                    Self::no_key_store_err().into()
-                })
-        }
+        self.sequoia.key_store_path_or_else()
     }
 
     /// Returns the key store.
@@ -268,35 +228,14 @@ impl Sq {
     /// If the key store is disabled, returns `Ok(None)`.  If it is not yet
     /// open, opens it.
     pub fn key_store(&self) -> Result<Option<&Mutex<keystore::Keystore>>> {
-        if self.no_key_store() {
-            return Ok(None);
-        }
-
-        self.key_store
-            .get_or_try_init(|| {
-                let mut c = keystore::Context::configure()
-                    .home(self.key_store_path_or_else()?);
-
-                if let Some(p) = self.config.servers_path() {
-                    c = c.lib(p);
-                }
-
-                let c = c.build()?;
-                let ks = keystore::Keystore::connect(&c)
-                    .context("Connecting to key store")?;
-
-                Ok(Mutex::new(ks))
-            })
-            .map(Some)
+        self.sequoia.key_store()
     }
 
     /// Returns the key store.
     ///
     /// If the key store is disabled, returns an error.
     pub fn key_store_or_else(&self) -> Result<&Mutex<keystore::Keystore>> {
-        self.key_store().and_then(|key_store| key_store.ok_or_else(|| {
-            Self::no_key_store_err().into()
-        }))
+        self.sequoia.key_store_or_else()
     }
 
     /// Returns the secret keys found in any specified keyrings.

lib/Cargo.toml
@@ -21,9 +21,11 @@ maintenance = { status = "actively-developed" }
 anyhow = "1.0.18"
 dirs = "5"
 filetime = "0.2"
+once_cell = "1.17"
 rusqlite = ">=0.31, <0.33"
 sequoia-cert-store = { version = "0.7", default-features = false }
 sequoia-directories = "0.1"
+sequoia-keystore = { version = "0.7" }
 sequoia-ipc = { version = "0.36", default-features = false }
 sequoia-openpgp = { version = "2", default-features = false, features = ["compression"] }
 sequoia-wot = { version = "0.15", default-features = false }

lib/src/builder.rs
@@ -49,8 +49,14 @@ pub struct SequoiaBuilder {
     /// Additional keyrings to read.
     keyrings: Vec<PathBuf>,
 
+    /// Overrides the path to the key store.
+    key_store_path: Option<crate::types::StateDirectory>,
+
     /// Additional trust roots.
     trust_roots: Vec<openpgp::Fingerprint>,
+
+    /// Path to our IPC servers.
+    ipc_server_path: PathBuf,
 }
 
 impl SequoiaBuilder {
@@ -62,7 +68,11 @@ impl SequoiaBuilder {
             time: Default::default(),
             cert_store_path: Default::default(),
             keyrings: Default::default(),
+            key_store_path: Default::default(),
             trust_roots: Default::default(),
+            ipc_server_path:
+            PathBuf::from(option_env!("PREFIX").unwrap_or("/usr/local"))
+                .join("libexec").join("sequoia"),
         }
     }
 
@@ -151,6 +161,12 @@ impl SequoiaBuilder {
         self
     }
 
+    /// Overrides the key store location.
+    pub fn key_store_path(&mut self, p: StateDirectory) -> &mut Self {
+        self.key_store_path = Some(p);
+        self
+    }
+
     /// Adds the given keyring to Sequoia's virtual cert store.
     pub fn add_keyring<P>(&mut self, p: P) -> &mut Self
     where
@@ -187,6 +203,15 @@ impl SequoiaBuilder {
         self
     }
 
+    /// Sets the path to the IPC servers.
+    pub fn ipc_server_path<P>(&mut self, p: P) -> &mut Self
+    where
+        P: AsRef<Path>,
+    {
+        self.ipc_server_path = p.as_ref().into();
+        self
+    }
+
     /// Instantiate a new context based on the builder's
     /// configuration.
     pub fn build(&self) -> Result<Sequoia> {
@@ -211,8 +236,11 @@ impl SequoiaBuilder {
             cert_store: Default::default(),
             keyrings: self.keyrings.clone(),
             keyring_tsks: Default::default(),
+            key_store_path: self.key_store_path.clone(),
+            key_store: Default::default(),
             trust_roots: self.trust_roots.clone(),
             trust_root_local: Default::default(),
+            ipc_server_path: self.ipc_server_path.clone(),
         })
     }
 }

lib/src/lib.rs
@@ -4,7 +4,7 @@ use std::{
     borrow::Cow,
     collections::BTreeMap,
     path::{Path, PathBuf},
-    sync::{Arc, OnceLock},
+    sync::{Arc, Mutex, OnceLock},
     time::SystemTime,
 };
 
@@ -18,6 +18,7 @@ use anyhow::Context;
 pub use anyhow;
 pub use sequoia_cert_store as cert_store;
 pub use sequoia_directories as directories;
+pub use sequoia_keystore as key_store;
 pub use sequoia_ipc as ipc;
 pub use sequoia_openpgp as openpgp;
 pub use sequoia_wot as wot;
@@ -76,6 +77,13 @@ pub struct Sequoia {
 
     /// Additional trust roots.
     trust_roots: Vec<Fingerprint>,
+
+    /// The key store.
+    key_store_path: Option<StateDirectory>,
+    key_store: once_cell::sync::OnceCell<Mutex<key_store::Keystore>>,
+
+    /// Path to our IPC servers.
+    ipc_server_path: PathBuf,
 }
 
 impl Sequoia {
@@ -386,6 +394,84 @@ impl Sequoia {
         }
     }
 
+    /// Returns the key store's path.
+    ///
+    /// If the key store is disabled, returns `Ok(None)`.
+    pub fn key_store_path(&self) -> Result<Option<PathBuf>> {
+        let default = || {
+            Ok(self.home()
+               .map(|h| h.data_dir(directories::Component::Keystore)))
+        };
+
+        if let Some(dir) = self.key_store_path.as_ref() {
+            match dir {
+                StateDirectory::Absolute(p) => Ok(Some(p.clone())),
+                StateDirectory::Default => default(),
+                StateDirectory::None => Ok(None),
+            }
+        } else {
+            default()
+        }
+    }
+
+    /// Returns whether the key store is disabled.
+    fn no_key_store(&self) -> bool {
+        self.key_store_path.as_ref()
+            .map(|s| s.is_none())
+            .unwrap_or(self.stateless())
+    }
+
+    fn no_key_store_err() -> errors::Error {
+        errors::Error::state_disabled("key store")
+    }
+
+    /// Returns the key store's path.
+    ///
+    /// If the key store is disabled, returns an error.
+    pub fn key_store_path_or_else(&self) -> Result<PathBuf> {
+        if self.no_key_store() {
+            Err(Self::no_key_store_err().into())
+        } else {
+            self.key_store_path()?
+                .ok_or_else(|| {
+                    Self::no_key_store_err().into()
+                })
+        }
+    }
+
+    /// Returns the key store.
+    ///
+    /// If the key store is disabled, returns `Ok(None)`.  If it is not yet
+    /// open, opens it.
+    pub fn key_store(&self) -> Result<Option<&Mutex<key_store::Keystore>>> {
+        if self.no_key_store() {
+            return Ok(None);
+        }
+
+        self.key_store
+            .get_or_try_init(|| {
+                let c = key_store::Context::configure()
+                    .home(self.key_store_path_or_else()?)
+                    .lib(self.ipc_server_path())
+                    .build()?;
+
+                let ks = key_store::Keystore::connect(&c)
+                    .context("Connecting to key store")?;
+
+                Ok(Mutex::new(ks))
+            })
+            .map(Some)
+    }
+
+    /// Returns the key store.
+    ///
+    /// If the key store is disabled, returns an error.
+    pub fn key_store_or_else(&self) -> Result<&Mutex<key_store::Keystore>> {
+        self.key_store().and_then(|key_store| key_store.ok_or_else(|| {
+            Self::no_key_store_err().into()
+        }))
+    }
+
     /// Returns the secret keys found in any specified keyrings.
     pub fn keyring_tsks(&self)
         -> Result<&BTreeMap<Fingerprint,
@@ -401,6 +487,11 @@ impl Sequoia {
             // map.
             Ok(self.keyring_tsks.get_or_init(|| BTreeMap::new()))
         }
+    }
+
+    /// Returns the path to the IPC servers.
+    pub fn ipc_server_path(&self) -> &Path {
+        &self.ipc_server_path
     }
 
     /// Emits a warning.

tool/Cargo.toml
@@ -34,10 +34,8 @@ futures-util = "0.3"
 gethostname = { version = ">=0.4, <0.6" }
 humantime = "2"
 indicatif = "0.17"
-once_cell = "1.17"
 reqwest = { version = ">=0.12, <0.13", features = ["hickory-dns", "stream"] }
 sequoia = { path = "../lib", default-features = false }
-sequoia-keystore = { version = "0.7" }
 tempfile = "3.1"
 thiserror = { version = ">=1, <3" }
 tokio = { version = "1.13.1" }

tool/src/commands/decrypt.rs
@@ -25,7 +25,7 @@ use sequoia::openpgp::types::KeyFlags;
 use sequoia::cert_store;
 use cert_store::store::StoreError;
 
-use sequoia_keystore as keystore;
+use sequoia::key_store as keystore;
 
 use crate::{
     cli,

tool/src/commands/key/list.rs
@@ -15,7 +15,7 @@ use sequoia::openpgp::{
     types::RevocationStatus,
 };
 
-use sequoia_keystore as keystore;
+use sequoia::key_store as keystore;
 use keystore::Protection;
 
 use crate::Convert;
@@ -546,7 +546,7 @@ pub fn list(sq: Sq, mut cmd: cli::key::list::Command) -> Result<()> {
         let mut hint = sq.hint(format_args!(
             "There are no secret keys."));
 
-        if sq.key_store_path.is_some()
+        if sq.sequoia.key_store_path()?.is_some()
             || ! sq.sequoia.home()
             .map(|h| h.is_default_location()).unwrap_or(false)
         {
@@ -555,7 +555,7 @@ pub fn list(sq: Sq, mut cmd: cli::key::list::Command) -> Result<()> {
                  using the `{}` option.  Consider using the default \
                  key store location to access your keys.",
                 sq.key_store_path()?.unwrap().display(),
-                if sq.key_store_path.is_some() {
+                if sq.sequoia.key_store_path()?.is_some() {
                     "--key-store"
                 } else {
                     "--home"

tool/src/common/key.rs
@@ -4,7 +4,7 @@ use openpgp::Result;
 use openpgp::cert::amalgamation::key::ValidErasedKeyAmalgamation;
 use openpgp::packet::key::KeyParts;
 
-use sequoia_keystore as keystore;
+use sequoia::key_store as keystore;
 
 use crate::Sq;
 use crate::cli::types::FileStdinOrKeyHandle;

commit e62f9d466701077e34b4b9071b27bc0abc60e9cf

tools: If the key store is not available, return a clap error.
  - If the key store is not available, return a clap error.  This
    causes `sq` to display the usage message.

  - Fixes b4a98c05d39035e947415c5ab1172280619084f3, which
    inadvertently removed this.

tool/src/sq.rs
@@ -179,11 +179,24 @@ impl Sq {
         self.sequoia.key_store()
     }
 
+    pub fn no_key_store_err() -> clap::Error {
+        clap::Error::raw(clap::error::ErrorKind::ArgumentConflict,
+                         "Operation requires a key store, \
+                          but the key store is disabled")
+    }
+
     /// Returns the key store.
     ///
     /// If the key store is disabled, returns an error.
     pub fn key_store_or_else(&self) -> Result<&Mutex<keystore::Keystore>> {
-        self.sequoia.key_store_or_else()
+        self.sequoia.key_store_or_else().map_err(|err| {
+            if let Some(err) = err.downcast_ref::<sequoia::Error>() {
+                if let sequoia::Error::StateDisabled { .. } = err {
+                    return Self::no_key_store_err().into();
+                }
+            }
+            err
+        })
     }
 
     /// Returns the secret keys found in any specified keyrings.